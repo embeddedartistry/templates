d8d87cebbf36d403deba02acc1db3102960d2a98
Michelle Luna Alama <89271104+milunadev@users.noreply.github.com>
2023-10-10 15:14:31 -0500

    Create funciones.py

---
+++ CodesPy/funciones.py
@@ -0,0 +1,6 @@
+# Este programa define una función para calcular el cuadrado de un número.
+def calcular_cuadrado(numero):
+    return numero ** 2
+
+resultado = calcular_cuadrado(5)
+print(f"El cuadrado de 5 es: {resultado}")
